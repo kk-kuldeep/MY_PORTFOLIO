507a68a6f0683cf355e66daff389184441b1a0b2
kk-kuldeep <ks13bro@gmail.com>
2022-07-05 01:12:05 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# My Portfolio
+
+This is my web-developer portfolio designed in HTML, & CSS while the data is added dynamically via JS.
+
+It lists all my projects.
+
+_Please feel free to check this repo out._
+
+_It would be great if you find any motivation for your next project, via this repo._
---
+++ scripts/projects.js
@@ -0,0 +1,67 @@
+const projects = [
+  {
+    name: "flag-mania",
+    date: "Aug, 21",
+    purpose: ["markOne"],
+    desc: "Want to know what all those flag emojis mean in your chats. Flag Mania can help you in getting a quick idea of what that flag represents, and whether you should engage in a conversation where the flag is being used.",
+    liveLink: "#",
+    srcLink: "https://github.com/kk-kuldeep/flag_mania",
+  },
+  {
+    name: "cash-register",
+    date: "Aug, 21",
+    purpose: ["markTwo"],
+    desc: "A web app that can help you to calculate how much minimum number of notes can be given when returning change.",
+    liveLink: "https://kk-kuldeep.github.io/cash_register/",
+    srcLink: "https://github.com/kk-kuldeep/cash_register",
+  },
+  {
+    name: "lucky-birthday",
+    date: "Aug, 21",
+    purpose: ["markThree"],
+    desc: "A web app that tells you if your birthday is lucky or not.",
+    liveLink: "https://kk-kuldeep.github.io/Lucky_birthday/",
+    srcLink: "https://github.com/kk-kuldeep/Lucky_birthday",
+  },
+];
+
+const content = document.querySelector("#primary-content");
+
+projects.forEach((project, index) => {
+  const purposesDiv = document.createElement("div");
+  project.purpose.forEach((purpose) => {
+    purposesDiv.innerHTML += `<span class="project-purpose">${purpose}</span>&nbsp;`;
+  });
+  const projectDiv = document.createElement("div");
+  projectDiv.innerHTML = `
+    <section class="container-centered">
+      <div class="project-section">
+        <h2 class="project-section__name">${project.name}</h2>
+        <small class="project-section__date">${project.date}</small>&emsp;<div class="purpose-div">${purposesDiv.innerHTML}</div>
+        <p class="project-section__desc">
+          ${project.desc}
+        </p>
+        <a
+          class="link--primary"
+          target="_blank"
+          rel="noreferrer noopener"
+          href="${project.liveLink}"
+          >Live Project</a
+        >
+        <br />
+        <a
+          class="link--secondary"
+          target="_blank"
+          rel="noreferrer noopener"
+          href="${project.srcLink}"
+          >View Source</a
+        >
+      </div>
+    </section>
+    `;
+  content.appendChild(projectDiv);
+  if (index !== projects.length - 1)
+    content.innerHTML += `
+    <hr class="hr--half" />
+    `;
+});
