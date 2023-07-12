29e3a7006ed6637ad74dfe3341c387ebc623500f
kuldeep singhal <97077977+kk-kuldeep@users.noreply.github.com>
2023-07-12 07:06:54 +0530

    Update projects live link

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,8 +1,16 @@
 const projects = [
   {
+    name: "Amazon_Clone",
+    date: "July, 23",
+    purpose: ["markZero"],
+    desc: "Give a try on Amazon clone . Learnt a lot of things while making this project",
+    liveLink: "amazon-clone-mul8.onrender.com",
+    srcLink: "https://github.com/kk-kuldeep/amazon_clone",
+  },
+  {
     name: "Portfolio",
     date: "July, 22",
-    purpose: ["markZero"],
+    purpose: ["markOne"],
     desc: "A web app that tells my portfolio.",
     liveLink: "https://kk-kuldeep.github.io/MY_PORTFOLIO/",
     srcLink: "https://github.com/kk-kuldeep/MY_PORTFOLIO",
@@ -10,23 +18,23 @@ const projects = [
   {
     name: "Disney_Clone",
     date: "July, 22",
-    purpose: ["markOne"],
+    purpose: ["markTwo"],
     desc: "Give a try on disney clone . Learnt a lot of things while making this project",
-    liveLink: "https://hotstar-clone-2.herokuapp.com/",
+    liveLink: "hotstar-clone-hr40.onrender.com/",
     srcLink: "https://github.com/kk-kuldeep/Hotstar_clone",
   },
   {
     name: "GIthub_user_finder",
     date: "Aug, 22",
-    purpose: ["markTwo"],
+    purpose: ["markThree"],
     desc: "Built an program to find user with their username. Used GitHub Api to search users and react js for frontend",
-    liveLink: "https://github-user-finder1.herokuapp.com/",
+    liveLink: "github-user-finder-y19g.onrender.com/",
     srcLink: "https://github.com/kk-kuldeep/github_user_finder",
   },
   {
     name: "Flag-mania",
     date: "July, 22",
-    purpose: ["markThree"],
+    purpose: ["markFour"],
     desc: "Want to know what all those flag emojis mean in your chats. Flag Mania can help you in getting a quick idea of what that flag represents, and whether you should engage in a conversation where the flag is being used.",
     liveLink: "https://prismatic-centaur-0cb2d8.netlify.app/",
     srcLink: "https://github.com/kk-kuldeep/flag_mania",
@@ -34,7 +42,7 @@ const projects = [
   {
     name: "Cash-register",
     date: "July, 22",
-    purpose: ["markFour"],
+    purpose: ["markFive"],
     desc: "A web app that can help you to calculate how much minimum number of notes can be given when returning change.",
     liveLink: "https://kk-kuldeep.github.io/cash_register/",
     srcLink: "https://github.com/kk-kuldeep/cash_register",
@@ -42,7 +50,7 @@ const projects = [
   {
     name: "Lucky-birthday",
     date: "July, 22",
-    purpose: ["markFive"],
+    purpose: ["markSix"],
     desc: "A web app that tells you if your birthday is lucky or not.",
     liveLink: "https://kk-kuldeep.github.io/Lucky_birthday/",
     srcLink: "https://github.com/kk-kuldeep/Lucky_birthday",
@@ -50,7 +58,7 @@ const projects = [
   {
     name: "Watch",
     date: "jan, 22",
-    purpose: ["markSix"],
+    purpose: ["markSeven"],
     desc: "a special watch with buttons that start, stop, and then zero the hands, used to time races.",
     liveLink: "https://kk-kuldeep.github.io/watch/",
     srcLink: "https://github.com/kk-kuldeep/watch",
@@ -58,7 +66,7 @@ const projects = [
   {
     name: "Leetcode Automation",
     date: "jan, 22",
-    purpose: ["markSeven"],
+    purpose: ["markEight"],
     desc: "Hey folks! Getting bored of copy paste codes at leetcode ? so here is the ultimate solution....... Leetcode Automation.",
     liveLink: "https://www.linkedin.com/posts/kuldeep-singhal-b81808234_project-puppeteer-op-activity-6923116920850264064-BEos?utm_source=linkedin_share&utm_medium=member_desktop_web",
     srcLink: "https://github.com/kk-kuldeep/web-development/tree/main/Node/4.automation/leetcode",
